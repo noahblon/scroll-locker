5d9d7b7d68fcd898108edd68f3c478d9f66110a6
Noah Blon <noahblon@gmail.com>
2017-01-21 18:12:16 -0600

    chore(ci): add some logs.

--- config/nightwatch/custom_commands/customSauceEnd.js
+++ config/nightwatch/custom_commands/customSauceEnd.js
@@ -1,11 +1,12 @@
 exports.command = function(cb) {
+    console.log(cb);
     var SauceLabs = require("saucelabs");
-
+    console.log(SauceLabs);
     var saucelabs = new SauceLabs({
         username: process.env.SAUCE_USERNAME,
         password: process.env.SAUCE_ACCESS_KEY
     });
-
+    console.log(saucelabs);
     var sessionid = this.capabilities['webdriver.remote.sessionid'];
     var jobName = this.currentTest.name;
 
